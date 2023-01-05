88c54b0a6ed7c93b90f5609f5e368825ff7aaeae
platosha-git <15platosha@gmail.com>
2023-01-05 18:40:49 +0300

    split dockerfiles into steps

--- scripts/build-images.sh
+++ scripts/build-images.sh
@@ -1,6 +1,6 @@
 version=1.0
 
-docker buildx build --push -t platoshagit/gateway-service:$version    --platform=linux/amd64 -f ./CarRentalSystem/APIGateway/Dockerfile .
+docker buildx build --push -t platoshagit/gateway-service:$version --platform=linux/amd64 -f ./CarRentalSystem/APIGateway/Dockerfile .
 docker buildx build --push -t platoshagit/car-service:$version --platform=linux/amd64 -f ./CarRentalSystem/Cars/Dockerfile .
 docker buildx build --push -t platoshagit/rental-service:$version --platform=linux/amd64 -f ./CarRentalSystem/Rentals/Dockerfile .
 docker buildx build --push -t platoshagit/payment-service:$version  --platform=linux/amd64 -f ./CarRentalSystem/Payments/Dockerfile .
